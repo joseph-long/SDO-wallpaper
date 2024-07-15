5a116f211836bfd08997825db6237cdddd94fdd6
Joseph Long <github@joseph-long.com>
2024-07-15 15:52:03 -0400

    Refactor for multiple displays, padded images

--- main.py
+++ main.py
@@ -1,36 +1,96 @@
-import subprocess
+import sys
+import random
+import AppKit
+from bs4 import BeautifulSoup
 import requests
 from pathlib import Path
+from PIL import Image
+import objc
 
-temp_path = Path.home() / "Downloads" / "sdo-osx-wallpaper.jpg"
+BASE_URL = "https://sdo.gsfc.nasa.gov"
+
+DOWNLOADS_PATH = Path.home() / "Downloads" / "current-sdo-images"
+
+def set_wallpaper(image_path : Path, screen: AppKit.NSScreen):
+    ws = AppKit.NSWorkspace.sharedWorkspace()
+    url = AppKit.NSURL.fileURLWithPath_(image_path.as_posix())
+    options = {}
+    error = objc.nil
+
+    ws.setDesktopImageURL_forScreen_options_error_(url, screen, options, error)
+
+    if error:
+        raise RuntimeError("Error in PyObjC setting desktop image: {error}")
 
 
-def change_wallpaper(image_path):
-    script = f"""
-    tell application "System Events"
-        tell current desktop
-            set picture to POSIX file "{image_path}"
-            set picture rotation to false
-        end tell
-    end tell
-    """
-    subprocess.run(["osascript", "-e", script])
+def resize_and_center_image(
+    image_path: Path,
+    output_path: Path,
+    final_width: int,
+    final_height: int,
+    background_color: tuple[int, int, int]
+):
+    # Load the original image
+    original_image = Image.open(image_path)
+
+    # Resize image if it is larger than the final dimensions
+    final_size = int(final_width), int(final_height)
+    original_image.thumbnail(final_size, Image.Resampling.LANCZOS)
+
+    # Calculate the padding
+    original_width, original_height = original_image.size
+    padding_left = int((final_width - original_width) // 2)
+    padding_top = int((final_height - original_height) // 2)
+
+    # Create new image with background color
+    new_image = Image.new("RGB", final_size, background_color)
+
+    # Paste original image onto the center of the new image
+    new_image.paste(original_image, (padding_left, padding_top))
+
+    # Save the result
+    new_image.save(output_path)
+    return output_path
 
 
-def download_latest_image(url=None):
+def download(url : str, dest : Path):
     """
     download image and wait for download to finish
     """
-    if url is None:
-        url = "https://sdo.gsfc.nasa.gov/assets/img/latest/latest_4096_HMIIC.jpg"
     response = requests.get(url, stream=True)
-    with open(temp_path, "wb") as file:
+    with open(dest, "wb") as file:
         for chunk in response.iter_content(chunk_size=128):
             file.write(chunk)
-    return temp_path
+    return dest
+
+
+def main():
+    bs = BeautifulSoup(requests.get(BASE_URL + "/data/").text, features="lxml")
+    srcs = [
+        x.attrs["href"]
+        for x in bs.find_all("a", href=lambda x: x and "latest_4096" in x)
+    ]
+
+    dest = DOWNLOADS_PATH / "padded"
+    dest.mkdir(exist_ok=True)
+
+    screens = AppKit.NSScreen.screens()
+    srcs = random.sample(srcs, k=len(screens))
+    for screen, src in zip(screens, srcs):
+        name = Path(src).name
+        dimensions = screen.devicePixelCounts()
+        dl_path = download(f"{BASE_URL}/{src}", DOWNLOADS_PATH / name)
+        padded_img = resize_and_center_image(
+            dl_path,
+            dest / name,
+            dimensions.width,
+            dimensions.height,
+            background_color=(0, 0, 0),
+        )
+        set_wallpaper(padded_img, screen)
+        print(f"{screen.localizedName()} set to {name}")
+    return 0
 
 
 if __name__ == "__main__":
-
-    download_latest_image()
-    change_wallpaper(temp_path)
+    sys.exit(main())
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-Scripts to update osx wallpaper to the latest [Solar Dynamics Observatory](https://sdo.gsfc.nasa.gov/) image.
+# Solar Dynamics Observatory living wallpaper
+
+Get a randomized one of the [Solar Dynamics Observatory](https://sdo.gsfc.nasa.gov/data/) latest images as your desktop background every time you run it.
